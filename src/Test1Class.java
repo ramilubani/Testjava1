e21ed8909428f75934ea6f68302025157bb1f09f
ramilubani <ramilubani@yahoo.com>
2025-04-05 01:31:20 +0200

    sdfsdf

--- src/Test1Class.java
+++ src/Test1Class.java
@@ -1,5 +1,7 @@
 public class Test1Class {
     public static void main(String[] args) {
-        System.out.println("test1class");
+        System.out.println("test44444444444441class");
+        System.out.println("change from windows");
+
     }
 }
