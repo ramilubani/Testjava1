6fd4595adb931553841d13b4d4afe8e2200c8abd
ramilubani <106825937+ramilubani@users.noreply.github.com>
2025-04-05 00:32:23 +0200

    Update Test2inTestJava1Project.java

--- src/Test2inTestJava1Project.java
+++ src/Test2inTestJava1Project.java
@@ -2,5 +2,6 @@ public class Test2inTestJava1Project {
     public static void main(String[] args) {
         System.out.println("test2inTestJava1Project");
         System.out.println("add new commit ");
+        System.out.println("add new commit2222 ");
     }
 }
