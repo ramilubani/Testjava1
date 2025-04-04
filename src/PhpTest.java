636f56fe52b7b54a9534abf32aa9b015fe744d1f
ramilubani <ramilubani@yahoo.com>
2025-04-05 01:18:38 +0200

    php

---
+++ src/PhpTest.java
@@ -0,0 +1,11 @@
+public class PhpTest {
+    public static void main(String[] args) {
+        System.out.println("sdfsdf");
+
+        System.out.println("Number of arguments: " + args.length);
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + i + ": " + args[i]);
+        }
+    }
+}
