8c60bcf764ac28e93a0f195fcb4fa7ce29f894e7
ramilubani <ramilubani@yahoo.com>
2025-04-05 00:29:38 +0200

    sdfsdf

--- src/Test1Class.java
+++ src/Test1Class.java
@@ -1,2 +1,5 @@
 public class Test1Class {
+    public static void main(String[] args) {
+        System.out.println("test1class");
+    }
 }
---
+++ src/Test2inTestJava1Project.java
@@ -0,0 +1,5 @@
+public class Test2inTestJava1Project {
+    public static void main(String[] args) {
+        System.out.println("test2inTestJava1Project");
+    }
+}
