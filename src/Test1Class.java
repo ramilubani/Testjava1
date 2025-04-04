ad7ac39201709a2025a3f5b09a04651b04d5fc2b
ramilubani <ramilubani@yahoo.com>
2025-04-05 01:32:51 +0200

    lkjsdflkjsdfklj

--- src/Test1Class.java
+++ src/Test1Class.java
@@ -1,7 +1,7 @@
 public class Test1Class {
     public static void main(String[] args) {
         System.out.println("test44444444444441class");
-        System.out.println("change from windows");
+        System.out.println("change from windowsdfgdfgdfg");
 
     }
 }
